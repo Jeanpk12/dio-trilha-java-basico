9b8b4d0e8b63a8c56789816bdbd46ca6dd8c2f9b
Jeanpk12 <jeanolivera123@gmail.com>
2024-07-22 10:17:49 -0300

    new commit

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String meuNome = "Jean"; // Tipo string armazena sequancia de caracteres
+        char tipoSanguinio = 'O'; // Tipo char armazena um caracter
+        int idade = 28; // Tipo int armazena número inteiro
+        double altura = 1.83; // Tipo double armazena números fracionados
+        boolean casado = false; // Tipo boolean armazena valores lógicos
+        // Existem outros tipos de dados primitivos, mas esses são os mais utilizados no dia a dia
     }
 }
