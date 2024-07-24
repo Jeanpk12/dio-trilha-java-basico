518d1b30894c02c0e4a101b691513288376b8b4b
Jeanpk12 <jeanolivera123@gmail.com>
2024-07-23 22:46:37 -0300

    fix commit

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -7,7 +7,6 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        DecimalFormat speedPattern = new DecimalFormat("00.00");
 
         System.out.println("Por favor, Digite o Nome do Cliente");
         String nomeCliente = scanner.next();
@@ -15,11 +14,10 @@ public class ContaTerminal {
         String agencia = scanner.next();
         System.out.println("Por favor, digite o número da conta");
         int conta = scanner.nextInt();
-        System.out.println("Por favor, digite o valor do primeiro dedósito");
+        System.out.println("Por favor, digite o valor do primeiro depósito");
         BigDecimal saldo = scanner.nextBigDecimal();
 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
-                + agencia + ", conta " + conta + " e seu saldo " + speedPattern.format(saldo)
-                + " já disponível para saque.");
+                + agencia + ", conta " + conta + " e seu saldo " + (saldo) + " já disponível para saque.");
     }
 }
